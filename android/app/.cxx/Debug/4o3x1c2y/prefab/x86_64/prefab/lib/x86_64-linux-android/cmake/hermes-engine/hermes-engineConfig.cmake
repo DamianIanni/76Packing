0afbc2ba332bc3d95e9a67c6e3f6c20d974272b7
Damian Ianni <iannigabrieldamian@gmail.com>
2025-04-28 21:04:14 -0300

    Cleanup: Remove iOS build and user-specific files from tracking

--- android/app/.cxx/Debug/4o3x1c2y/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/damianianni/.gradle/caches/8.10.2/transforms/f4a05e601a3df3c9f0a5859c28121dc5/transformed/hermes-android-0.77.0-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/damianianni/.gradle/caches/8.10.2/transforms/f4a05e601a3df3c9f0a5859c28121dc5/transformed/hermes-android-0.77.0-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- ios/Pods/Target Support Files/Pods-Awesome76/Pods-Awesome76-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Awesome76 : NSObject
-@end
-@implementation PodsDummy_Pods_Awesome76
-@end
